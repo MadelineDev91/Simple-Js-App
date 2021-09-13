b2052cd4aabd36e10f536edb5a0da07bcbfa7dfb
MadelineDev91 <89571777+MadelineDev91@users.noreply.github.com>
2021-09-13 15:09:09 +0200

    Updated script
    Created a loop with a conditional for height

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,17 @@
 // array of objects
 
 let pokemonList = [
-  { name: 'Butterfree', height: 1, type: ['honey', 'flowers']},
+  { name: 'Butterfree', height: 3, type: ['honey', 'flowers']},
   { name: 'Pidgeotto', height: 4, type: ['grass', 'bugs']},
-  { name: 'Pikachu', height: 5, type: ['steel', 'electric']}
+  { name: 'Pikachu', height: 9, type: ['steel', 'electric']}
 ];
 
-console.log(pokemonList);
+for (let i=0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height <4 && pokemonList[i].height >1) {
+    document.write(pokemonList[i].name + ", height - " + pokemonList[i].height + " :This is a small Pokemon" + "<br>");
+  }else if (pokemonList[i].height <5){
+    document.write(pokemonList[i].name + ", height - " + pokemonList[i].height + " :This is a medium Pokemon" + "<br>");
+  }else {
+    document.write(pokemonList[i].name + ", height - " +  pokemonList[i].height + " :Wow, this is a big Pokemon!" + "<br>");
+  }
+}
